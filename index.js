f185f8131b5ed0fbd102f5bf4258010131b27cff
nggigi <ngigi.francis5@gmail.com>
2024-01-06 09:33:33 -0500

    completed

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your code here!
+
+
+
+let mainEl= document.querySelector('main#main')
+mainEl.remove()
+
+const newHeader = document.createElement("h1")
+newHeader.id ="victory"
+newHeader.innerHTML="is the champion"
+
+
+
+//       
+  
+//      
+//       it("the 'newHeader' variable that points to the <h1> node with an id of 'victory' has the text \"YOUR-NAME is the champion\" (with your name in place of YOUR-NAME) inside it", () => {
+//         expect(newHeader.innerHTML, "Make sure your <h1> with id 'victory' includes the text \"is the champion\" inside it").to.include("is the champion");
+//       });
+  
+//     });
+//   })
+  
